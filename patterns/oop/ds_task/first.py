5f2983d763aea561822b0f13fe3c8b865ce7fa17
d.valov <valovdaniil2003@gmail.com>
2025-04-03 14:45:14 +0300

    jobVS

---
+++ patterns/oop/ds_task/first.py
@@ -0,0 +1,29 @@
+import asyncio
+
+
+class Bear:
+    def __init__(self, country: str, color: str):
+        self.country = country
+        self.color = color
+
+    async def dance(self):
+        print(f'{self.country} танец')
+
+    async def hug(self):
+        print(f'пушистые, {self.color} объятия')
+
+
+async def main():
+    bear_list = {
+        'белый': 'антарктический',
+        'бурый': 'русский'
+    }
+
+    bears_party = [Bear(country, color).dance() for color, country in bear_list.items()]
+    
+
+    await asyncio.gather(*bears_party)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
